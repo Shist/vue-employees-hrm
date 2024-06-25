dc6d8ed15e4a32e4026a5bed510d4e84c95e5f3a
Shist <shist.pupust@mail.ru>
2024-06-25 10:13:18 +0300

    Added bread crumbs component, first pinia store, vuetify config & icons

---
+++ src/components/BreadCrumbs.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-breadcrumbs :items="items" divider="$next" class="bread-crumbs">
+    <template v-slot:prepend>
+      <v-icon>mdi-home-outline</v-icon>
+    </template>
+    <template v-slot:divider>
+      <v-icon>mdi-chevron-right</v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+import { useThemeStore } from "@/store/useThemeStore";
+
+const theme = useThemeStore();
+
+const items = reactive([
+  {
+    title: "Home",
+    disabled: false,
+    href: "breadcrumbs_home",
+  },
+  {
+    title: "Employees",
+    disabled: true,
+    href: "breadcrumbs_employees",
+  },
+]);
+</script>
+
+<style lang="scss" scopped>
+.bread-crumbs {
+  font-family: $font-roboto;
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="main-page">
+    <BreadCrumbs />
     <ExampleComponent />
   </div>
 </template>
 
 <script setup lang="ts">
+import BreadCrumbs from "@/components/BreadCrumbs.vue";
 import ExampleComponent from "@/components/ExampleComponent.vue";
 </script>
 
