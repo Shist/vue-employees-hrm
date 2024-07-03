c407c7b5d78a3d39b4d4bd34c8e4d4904f72fe35
Shist <shist.pupust@mail.ru>
2024-07-03 13:07:36 +0300

    Departments data interface was refactored to the real server interface

--- src/store/departments.ts
+++ src/store/departments.ts
@@ -1,43 +1,52 @@
 import { defineStore } from "pinia";
 import { reactive, Reactive } from "vue";
-import { IDepartmentData } from "@/types/departmentData";
+import { IDepartment } from "@/types/backend-interfaces/department";
 
 export const useDepartmentsStore = defineStore("departments", () => {
-  const departments: Reactive<IDepartmentData[]> = reactive([
+  const departments: Reactive<IDepartment[]> = reactive([
     {
       id: 1,
+      created_at: "03.07.2024",
       name: ".Net",
     },
     {
       id: 2,
+      created_at: "03.07.2024",
       name: "Blockchain",
     },
     {
       id: 3,
+      created_at: "03.07.2024",
       name: "DevOps",
     },
     {
       id: 4,
+      created_at: "03.07.2024",
       name: "Global",
     },
     {
       id: 5,
+      created_at: "03.07.2024",
       name: "Java",
     },
     {
       id: 6,
+      created_at: "03.07.2024",
       name: "Node",
     },
     {
       id: 7,
+      created_at: "03.07.2024",
       name: "Python",
     },
     {
       id: 8,
+      created_at: "03.07.2024",
       name: "Quality Assurance",
     },
     {
       id: 9,
+      created_at: "03.07.2024",
       name: "React",
     },
   ]);
