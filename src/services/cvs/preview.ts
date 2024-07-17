8cc0a621151a102055478696fe961f656fb09e66
Shist <shist.pupust@mail.ru>
2024-07-17 12:09:06 +0300

    Implemented Apollo GraphQL mutation and service function for export PDF

--- src/services/cvs/preview.ts
+++ src/services/cvs/preview.ts
@@ -1,7 +1,9 @@
 import apolloClient from "@/plugins/apolloConfig";
 import getCVPreviewDataByIDQuery from "@/graphql/cvs/preview/getCVPreviewDataByID.query.gql";
+import exportPDFQuery from "@/graphql/cvs/preview/exportPDF.mutation.gql";
 import { checkCvID, getDetailedError } from "@/utils/handleErrors";
 import { ICVPreviewServerData } from "@/types/cvPreviewUI";
+import { IExportPDFInput } from "@/types/backend-interfaces/cv/exportPDFInput";
 
 export const getCVPreviewDataByID = async (id: string) => {
   try {
@@ -13,6 +15,22 @@ export const getCVPreviewDataByID = async (id: string) => {
     })) as { data: { cv: ICVPreviewServerData } };
 
     return response.data.cv;
+  } catch (error: unknown) {
+    throw getDetailedError(error);
+  }
+};
+
+export const exportPDF = async (exportPDFInput: IExportPDFInput) => {
+  try {
+    console.log(exportPDFInput);
+    const response = (await apolloClient.query({
+      query: exportPDFQuery,
+      variables: { pdf: exportPDFInput },
+    })) as { data: { exportPdf: string } };
+
+    console.log(response.data.exportPdf);
+
+    return response.data.exportPdf;
   } catch (error: unknown) {
     throw getDetailedError(error);
   }
